0ffd4672894c73d73b6148b0efd198aebd58a539
JesseVrooman <0945181@hr.nl>
2021-01-11 13:26:04 +0100

    new Build

--- static/js/main.1ef5b6bd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n(1),s=n.n(r),p=n(3);var a=function(){return Object(c.jsx)("div",{className:"App",children:"Hello World!"})};n.n(p).a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.1ef5b6bd.chunk.js.map
