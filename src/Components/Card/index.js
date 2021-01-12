52608bd45580bb2b547eafe52d19207bf2480df2
JesseVrooman <0945181@hr.nl>
2021-01-12 13:54:03 +0100

    new build

---
+++ src/Components/Card/index.js
@@ -0,0 +1,66 @@
+export default function Card(data) {
+    return(
+        <figure class={"card card--" + data.type} style={{display: "flex" }}>
+                  <div class="card__image-container">
+                    <img
+                      src={process.env.PUBLIC_URL + "/assets/Team/" + data.name.split(" ")[0] + ".png"}
+                      alt="Person"
+                      class="card__image"
+                      style={{ 
+                        width: "100%",
+                        height: "auto",
+                        display: "inline-block"
+                        }}
+                    />
+                  </div>
+
+                  <figcaption class="card__caption">
+                    <h1 class="card__name">{data.name}</h1>
+
+                    <h3 class="card__type">{data.type}</h3>
+
+                    <table class="card__stats">
+                      <tbody>
+                        <tr>
+                          <th>HP</th>
+                          <td>{data.HP}</td>
+                        </tr>
+                        <tr>
+                          <th>Attack</th>
+                          <td>{data.ATK}</td>
+                        </tr>
+
+                        <tr>
+                          <th>Defense</th>
+                          <td>{data.DEF}</td>
+                        </tr>
+
+                        <tr>
+                          <th>Special Attack</th>
+                          <td>{data.HP}</td>
+                        </tr>
+                        <tr>
+                          <th>Special Defense</th>
+                          <td>{data.SPDEF}</td>
+                        </tr>
+                        <tr>
+                          <th>Speed</th>
+                          <td>{data.SPEED}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    <div class="card__abilities">
+                      <h4 class="card__ability">
+                        <span class="card__label">Ability</span>
+                        {data.Ability}
+                      </h4>
+                      <h4 class="card__ability">
+                        <span class="card__label">Hidden Ability</span>
+                        {data.HAbility}
+                      </h4>
+                    </div>
+                  </figcaption>
+                </figure>           
+    );
+}
--- src/Pages/AboutUs/index.js
+++ src/Pages/AboutUs/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { StandardLayout } from "../../Components/Layout/Layout";
 import "../../assets/Styling/main.css";
+import data from "../../assets/json/Team.json"
+import Card from "../../Components/Card";
 
 function AboutUsLayout() {
   return (
@@ -54,38 +56,19 @@ function TheTeam() {
       <section
         style={{
           width: "100%",
-          height: "420px",
           position: "relative",
           fontFamily: "Veneficium",
           backgroundColor: "rgb(30,30,30)",
           boxShadow: "inset 0px 0px 50px 10px black",
         }}
       >
-        <div
-          className="container"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ flexBasis: "100px", marginRight: "50px" }}>
-            <img src={process.env.PUBLIC_URL + "/assets/logo.png"} />
-          </div>
-          <div style={{ flexBasis: "500px" }}>
-            <p>
-              In Veneficium, you play as a witch-in-training called Alice. Alice
-              lives in a magical place called Diana's coven, and you will be
-              making a trip with her through her homeplace. At the start of the
-              story, she will learn that the witches in her coven have all gone
-              missing, and so the brave little witch decides it is her task to
-              go look for them. Throughout her journey, she will encounter many
-              troubled witches that she has to help. Not only this, but she also
-              learns more and more about the cause of all of these terrible
-              things. Will you help Alice on her journey to save the witches in
-              her coven?
-            </p>
-          </div>
+        <div className="container"style={{padding: 30}}>
+          <h1 style={{textAlign: "center", fontSize: "70px"}}>About Us</h1>
+          <p style={{fontSize: "30px", textAlign: "center"}}>
+          First of all, we would like to thank you for checking out our game! We are Frostpetal Studio,  a group of 15 students working on this game for a university project. All of us are very invested to bring you a great game, and hope you have fun playing it. We hope you stick with us as we work on Veneficium (and maybe other projects too, in the future!)
+          If you are interested in following our team more closely, check out our other socials too!
+
+          </p>
         </div>
       </section>
       <div
@@ -104,7 +87,6 @@ function TeamPics() {
       <section
         style={{
           width: "100%",
-          height: "600px",
           position: "relative",
           fontFamily: "Veneficium",
           backgroundColor: "white",
@@ -124,76 +106,18 @@ function TeamPics() {
             <h1 style={{ textAlign: "center", fontSize: 70 }}>The Team</h1>
             <div>
               <div style={{ flexBasis: "500px" }}>
-
-
-
-              <div id="cards" className="row justify-content-center" style={{maxWidth: "100%"}} >  
-                <figure class="card card--water" style={{display: "flex" }}>
-                  <div class="card__image-container">
-                    <img
-                      src="https://cdn.bulbagarden.net/upload/thumb/f/fd/134Vaporeon.png/1200px-134Vaporeon.png"
-                      alt="Vaporeon"
-                      class="card__image"
-                      style={{ 
-                        maxHeight: "150px",
-                        margin: "auto",
-                        display: "inline-block"
-                        }}
-                    />
-                  </div>
-
-                  <figcaption class="card__caption">
-                    <h1 class="card__name">Vaporeon</h1>
-
-                    <h3 class="card__type">water</h3>
-
-                    <table class="card__stats">
-                      <tbody>
-                        <tr>
-                          <th>HP</th>
-                          <td>130</td>
-                        </tr>
-                        <tr>
-                          <th>Attack</th>
-                          <td>65</td>
-                        </tr>
-
-                        <tr>
-                          <th>Defense</th>
-                          <td>60</td>
-                        </tr>
-
-                        <tr>
-                          <th>Special Attack</th>
-                          <td>110</td>
-                        </tr>
-                        <tr>
-                          <th>Special Defense</th>
-                          <td>95</td>
-                        </tr>
-                        <tr>
-                          <th>Speed</th>
-                          <td>65</td>
-                        </tr>
-                      </tbody>
-                    </table>
-
-                    <div class="card__abilities">
-                      <h4 class="card__ability">
-                        <span class="card__label">Ability</span>
-                        Absorb
-                      </h4>
-                      <h4 class="card__ability">
-                        <span class="card__label">Hidden Ability</span>
-                        Hydration
-                      </h4>
-                    </div>
-                  </figcaption>
-                </figure>               
-
+                <div id="cards" className="row justify-content-center" style={{maxWidth: "100%"}} >  
+                    {data.map((d, i) => i === 0 ? Card(d) : null)}
                 </div>
-
-
+                <div id="cards" className="row justify-content-center" style={{maxWidth: "100%"}} >  
+                    {data.map((d, i) => i >= 1  && i <= 3 ? Card(d) : null)}
+                </div>
+                <div id="cards" className="row justify-content-center" style={{maxWidth: "100%"}} >  
+                    {data.map((d, i) => i >= 4  && i <= 8 ? Card(d) : null)}
+                </div>
+                <div id="cards" className="row justify-content-center" style={{maxWidth: "100%"}} >  
+                    {data.map((d, i) => i >= 9  && i <= 15 ? Card(d) : null)}
+                </div>
               </div>
             </div>
           </div>
